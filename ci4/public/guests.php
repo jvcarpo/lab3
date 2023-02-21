ce7da475bea52c84c03db1d906273dd7d8f23ad6
Jarvis Carpo <jvcarpo@student.apc.edu.ph>
2023-02-21 20:22:17 +0800

    Week 11 ci4

---
+++ ci4/public/guests.php
@@ -0,0 +1,27 @@
+  <?php
+          $servername = "localhost";
+          $username = "webprogmi211";
+          $password = "webprogmi211";
+          $dbname = "webprogmi211";
+          
+          // Create connection
+          $conn = new mysqli($servername, $username, $password, $dbname);
+          // Check connection
+          if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+          }
+
+          $sql = "SELECT name, email, website, comment, gender FROM jvcarpo_myguests";
+          $result = $conn->query($sql);
+
+          if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+              echo "<br> Name: " . $row["name"]. " - Email: " . $row["email"]. " - Website: " . $row["website"].
+              " - Comment: " . $row["comment"]. " - Gender: " . $row["gender"]. "<br>";
+            }
+          } else {
+            echo "0 results";
+          }
+          $conn->close();  
+  ?>
